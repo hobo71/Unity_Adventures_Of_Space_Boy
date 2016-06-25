e7e7202a04f9afb2c21f35d5ce6074336a9c894a
Spider <silentwalk15@gmail.com>
2016-06-25 09:23:46 +0200

    Player can Move and Jump

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpSpeed;
+    private Rigidbody2D myRigidbody;
+
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveLeftandRight(moveSpeed);
+        jump(jumpSpeed);
+
+    }
+
+    private void moveLeftandRight(float moveSpeed)
+    {
+        // Right and Left movement using Axes from Input
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            myRigidbody.velocity = new Vector3(moveSpeed, myRigidbody.velocity.y, 0f);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            myRigidbody.velocity = new Vector3(-moveSpeed, myRigidbody.velocity.y, 0f);
+        }
+
+        // To prevent the player from sliding
+        else
+        {
+            myRigidbody.velocity = new Vector3(0f, myRigidbody.velocity.y, 0f);
+        }
+    }
+
+    private void jump(float jumpSpeed)
+    {
+        if(Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W))
+        {
+            myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jumpSpeed, 0);
+        }
+    }
+
+}
