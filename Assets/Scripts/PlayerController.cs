82a48be16a34ca3084d47e05d88dd8905367369b
Spider <silentwalk15@gmail.com>
2016-06-25 09:44:27 +0200

    Player can only jump if on ground

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,30 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    // Player related
     public float moveSpeed;
     public float jumpSpeed;
+
+    // For checking with collision
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    // To check what the player lands on, Ground, Enemy, Water, and so on
+    public LayerMask whatIsGround;
+    public bool isGrounded;
+
     private Rigidbody2D myRigidbody;
 
 	// Use this for initialization
 	void Start () {
+
+        // Gets the component which this script is attached to
         myRigidbody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        isGroundedMethod(groundCheck, groundCheckRadius, whatIsGround);
         moveLeftandRight(moveSpeed);
         jump(jumpSpeed);
 
@@ -40,10 +53,18 @@ public class PlayerController : MonoBehaviour {
 
     private void jump(float jumpSpeed)
     {
-        if(Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.W))
+        if(Input.GetButtonDown("Jump") && isGrounded || Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
             myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jumpSpeed, 0);
         }
+    }
+
+    private bool isGroundedMethod(Transform groundCheck, float groundCheckRadius, LayerMask whatIsGround)
+    {
+        // Creates overlap circle to check whether on ground or not.
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        return isGrounded;
     }
 
 }
